de02a4483545dbd0d07f2d6d3be319447bd57e57
TheoR <reynier.theo@gmail.com>
2026-06-20 19:45:00 +0200

    soleil

--- tests/will/test_space_ship.gd
+++ tests/will/test_space_ship.gd
@@ -7,8 +7,13 @@ var orbites : Dictionary
 # puissent continuer à l'utiliser sans planter
 @export var velocity := Vector2(200.0, 0.0)
 
+var in_range_sun = false
+var sun : RigidBody2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	sun = get_parent().get_parent().get_node("Soleil")
+	
 	# On désactive la gravité globale du projet pour ce vaisseau
 	gravity_scale = 0.0
 	
@@ -33,25 +38,38 @@ func _physics_process(_delta: float) -> void:
 func calculate_attractions() -> Vector2:
 	var force = Vector2(0.0, 0.0) 
 	
+	var dir_sun = (sun.global_position - global_position).normalized()
+	#var force_sun = dir_sun * sun.mass * 0.1 / global_position.distance_to(sun.global_position)
+	var force_sun = dir_sun * sun.mass * 0.0001 * (sun.get_node("GraviteArea/CollisionShape2D").shape.radius - global_position.distance_to(sun.global_position))
+	
+	if in_range_sun:
+		print("Je vais me bruler au soleil")
+		force += force_sun
+	
 	for i in orbites.keys():
 		var orbite = orbites[i]
 		# orbite[1] correspond à la position de la planète
 		var dir = (orbite[1] - global_position).normalized()
 		# orbite[0] correspond à l'intensité de la gravité de cette planète
-		force += dir * orbite[0]
+		force += dir * orbite[0] 
 		
 	return force
 
 
 # Ajoute l'orbite traversée au dictionnaire "orbites" avec le nom en clé et les infos en valeurs
 func _on_detection_area_area_entered(area: Area2D) -> void:
+	print(area)
 	var parent = area.get_parent()
+	print(parent)
 	
 	if parent.is_in_group("Planetes"):
 		var info = parent.get_info()
 		orbites[info[0]] = [info[1], info[2], info[3]]
 	elif parent.is_in_group("Destructibles"):
 		parent.hit()
+	elif parent.is_in_group("Morts"):
+		print("oui")
+		in_range_sun = true
 
 
 # Retire l'orbite qu'on vient de quitter
